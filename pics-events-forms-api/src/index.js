641dfe26bdcf21db1cb110d34c37f108d40ea145
Theodore Chuang <chuangtheodore@gmail.com>
2019-03-17 23:08:17 +1100

    PicsSearch: scaffold SearchBar, add Unsplash API

---
+++ pics-events-forms-api/src/index.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+dotenv.config();
+
+ReactDOM.render(<App />, document.querySelector("#root"));
