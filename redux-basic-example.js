501998b19e6667112ad6bae569398ab039b515a7
Theodore Chuang <chuangtheodore@gmail.com>
2019-07-22 22:39:36 +1000

    [react-redux-basic-song-list] Add and connect SongList, SongDetail

--- redux-basic-example.js
+++ redux-basic-example.js
@@ -1,4 +1,4 @@
-//// Redux Flow: Action Creator -> Action -> dispatch -> Reducers -> State ////
+//// Redux Flow: Action Creator -> Action -> dispatch (connect) -> Reducers -> State ////
 //// Example model after Insurance company
 
 //// Action Creators, return an Action ////
@@ -69,6 +69,9 @@ const store = createStore(ourDepartments);
 
 //// Example Usage ////
 
+// returns an object but not connected to state/store
+createPolicy("Alex", 20);
+
 store.dispatch(createPolicy("Alex", 20));
 store.dispatch(createPolicy("Bianca", 20));
 
@@ -77,3 +80,55 @@ store.dispatch(createClaim("Alex", 100));
 store.dispatch(deletePolicy("Bianca", 100));
 
 console.log(store.getState());
+
+//// Example 2: Counter ////
+
+// Action Creators - You don't need to change these
+const increment = () => ({ type: "increment" });
+const decrement = () => ({ type: "decrement" });
+
+const Counter = props => {
+  return (
+    <div>
+      <button onClick={props.increment} className="increment">
+        Increment
+      </button>
+      <button onClick={props.decrement} className="decrement">
+        Decrement
+      </button>
+      Current Count: <span>{props.count}</span>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { count: state.count };
+};
+
+const WrappedCounter = ReactRedux.connect(mapStateToProps, {
+  increment,
+  decrement
+})(Counter);
+
+const store = Redux.createStore(
+  Redux.combineReducers({
+    count: (count = 0, action) => {
+      if (action.type === "increment") {
+        return count + 1;
+      } else if (action.type === "decrement") {
+        return count - 1;
+      } else {
+        return count;
+      }
+    }
+  })
+);
+
+ReactDOM.render(
+  <ReactRedux.Provider store={store}>
+    <WrappedCounter />
+  </ReactRedux.Provider>,
+  document.querySelector("#root")
+);
+
+<div id="root" />;
