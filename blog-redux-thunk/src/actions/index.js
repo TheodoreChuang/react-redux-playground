4d33dbf705125c51683093252a3c0fd1cdda5b46
Theodore Chuang <chuangtheodore@gmail.com>
2019-08-15 23:12:56 +1000

    [blog-redux-thunk] Refactor compose ActionCreators

--- blog-redux-thunk/src/actions/index.js
+++ blog-redux-thunk/src/actions/index.js
@@ -2,6 +2,21 @@ import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 // redux-thunk middleware extends Action Creators to return functions
+
+// Alternative to memoize of fetchUser, Action Creators within Action Creators
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
@@ -11,16 +26,35 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
-});
+};
+
+// Working memoize version of fetchUser, not able to refetchUser
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data
+//   });
+// });
+
+// Works but makes many requests
+// export const fetchUser = id => async dispatch => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data
+//   });
+// };
 
 /*
   Issues with Async action creator: 
@@ -28,6 +62,7 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
   2. Promises fixes #1 but Action will reach Reducers before getting API data
   https://www.udemy.com/react-redux/learn/lecture/12586860#content
 */
+// This will not work
 // const response = await jsonPlaceholder.get("/posts");
 // return {
 //   type: "FETCH_POSTS",
--- blog-redux-thunk/src/components/UserHeader.js
+++ blog-redux-thunk/src/components/UserHeader.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
+  // componentDidMount() {
+  //   this.props.fetchUser(this.props.userId);
+  // }
 
   render() {
     const { user } = this.props;
@@ -27,5 +26,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchUser }
+  {}
 )(UserHeader);
