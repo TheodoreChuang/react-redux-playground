e5a908a0ed96bf620f9870ed0d509d6bd733617c
Theodore Chuang <chuangtheodore@gmail.com>
2019-11-18 21:13:21 +1100

    [router-auth-video-streamer][client] Add Redux. Refactor auth state to use redux store

--- router-auth-video-streamer/client/src/index.js
+++ router-auth-video-streamer/client/src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
 
 require("dotenv").config();
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
